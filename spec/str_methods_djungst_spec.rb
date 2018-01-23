56f6039df864a628e0e50e244ec858ca2367de72
David Jungst <djungst@gmail.com>
2018-01-22 18:56:24 -0700

    add str methods

--- spec/str_methods_djungst_spec.rb
+++ spec/str_methods_djungst_spec.rb
@@ -1,9 +1,20 @@
 RSpec.describe StrMethodsDjungst do
+  subject { StrMethodsDjungst }
+
   it "has a version number" do
     expect(StrMethodsDjungst::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'manipulate strings' do
+    let(:str) { 'My String' }
+
+    it 'reverses a string' do
+      expect(subject.reversify(str)).to eq('gnirtS yM')
+    end
+
+    it 'adds spaces' do
+      expect(subject.spacify(str, 1)).to eq('M y   S t r i n g')
+    end
   end
+
 end
